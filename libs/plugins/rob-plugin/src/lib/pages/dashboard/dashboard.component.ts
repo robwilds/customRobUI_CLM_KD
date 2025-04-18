85e1deb8564fc04a962d63a158234a95b683308c
Rob Wilds <robwilds@me.com>
2025-04-18 16:27:15 -0400

    updated kdsearch and dashboard pages to separate pages

--- libs/plugins/rob-plugin/src/lib/pages/dashboard/dashboard.component.ts
+++ libs/plugins/rob-plugin/src/lib/pages/dashboard/dashboard.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
-    template: `rob-plugin-dashboard Works! {{ 'PLUGIN_MESSAGE' | translate }}`,
-    selector: 'rob-plugin-dashboard',
-    imports: [TranslateModule],
-    standalone: true,
+  templateUrl: './dashboard.Component.html',
+  selector: 'rob-plugin-dashboard',
+  imports: [TranslateModule],
+  styleUrl: './dashboard.component.scss',
+  standalone: true,
 })
-export class DashboardComponent {
-}
+export class DashboardComponent {}
--- libs/plugins/rob-plugin/src/lib/pages/kdsearch/kdsearch.component.ts
+++ libs/plugins/rob-plugin/src/lib/pages/kdsearch/kdsearch.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
-    template: `rob-plugin-kdsearch Works! {{ 'PLUGIN_MESSAGE' | translate }}`,
-    selector: 'rob-plugin-kdsearch',
-    imports: [TranslateModule],
-    standalone: true,
+  templateUrl: './kdsearch.component.html',
+  selector: 'rob-plugin-kdsearch',
+  imports: [TranslateModule],
+  styleUrl: './kdsearch.component.scss',
+  standalone: true,
 })
-export class KdsearchComponent {
-}
+export class KdsearchComponent {}
